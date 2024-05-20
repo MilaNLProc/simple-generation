f1dfe407b2a907c2af03725c5407c0e6297e1793
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2024-05-20 23:23:16 +0200

    minor changes

--- simple_generation/__init__.py
+++ simple_generation/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Giuseppe Attanasio"""
 __email__ = "giuseppeattanasio6@gmail.com"
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 
 from .simple_generation import SimpleGenerator, DefaultGenerationConfig
