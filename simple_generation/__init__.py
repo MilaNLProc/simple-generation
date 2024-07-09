1b69523e6c5064f42a877f150bbe14c83e177537
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2024-07-09 11:35:28 +0200

    Bump version to 0.5.1

--- simple_generation/__init__.py
+++ simple_generation/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Giuseppe Attanasio"""
 __email__ = "giuseppeattanasio6@gmail.com"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 
 from .simple_generation import SimpleGenerator, DefaultGenerationConfig
